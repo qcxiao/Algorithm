6fad721201714e85559e811b5098a318de459bca
xiaoqichuan@juzix.net <xiaoqichuan@juzix.net>
2020-09-06 11:43:16 +0800

    update

---
+++ src/main/java/top/qcxiao/redblacktree/RedBlackTree.java
@@ -0,0 +1,17 @@
+package top.qcxiao.redblacktree;
+
+/**
+ * @title RedBlackTree
+ * @description 红黑树，为了解决二叉查找树多次插入新结点导致的不平衡而产生的
+ * @link https://mp.weixin.qq.com/s?__biz=MzIwMTAzMTMxMg==&mid=2649496793&idx=2&sn=6594afc3e8c34637295257dfa70842b8&chksm=8eec9726b99b1e30cd7fc0c7adf735e8c1c9228e7dc9cfc046ede688706f38d6ebce786b9a1b&mpshare=1&scene=1&srcid=&sharer_sharetime=1592356861960&sharer_shareid=d1ceaeecf9b1503790b4f3f4352644d8&key=8d9e1ac2792cb89ff870aec3acdabfa481aa849fb1d66658922f24f123e197752be8b728c4ff30a6a728a2308c7e2f4e7c39b927c9f13614fdf9822fd97130464a287186e85d342b159079145b179a1b&ascene=1&uin=MjY1MjU0MTU2NA%3D%3D&devicetype=Windows+10+x64&version=62090070&lang=zh_CN&exportkey=A2MGrTMIAXUseWM4jIS0nlo%3D&pass_ticket=7Pr44kJWsyXgU29FzsnF3AMHptnbzLkJqOvfbYKjlQaQCaMnoH6CfV3zB53LzSoA
+ * 1.结点是红色或黑色
+ * 2.根结点是黑色
+ * 3.每个叶子结点都是黑色的空结点（NIL结点）
+ * 4 每个红色结点的两个子结点都是黑色。(从每个叶子到根的所有路径上不能有两个连续的红色结点)
+ * 5.从任一结点到其每个叶子的所有路径都包含相同数目的黑色结点
+ * @author qcxiao
+ * @updateTime 2020/9/3 14:23
+ */
+public class RedBlackTree {
+
+}
